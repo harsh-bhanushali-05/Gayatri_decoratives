d1c2432b3b46a998e10c28201e224f02ea33dc33
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2023-11-12 14:47:40 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+
 require("dotenv").config();
 const express = require("express");
 const database = require("mongoose");
@@ -6,6 +7,18 @@ const products = require("./routes/products");
 app.use(express.json());
 app.use("/API/Products", products);
 const cors = require('cors');
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://65509721ac022f55419e8de0--fabulous-torte-c69744.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // Enable preflight requests
+    if (req.method === 'OPTIONS') {
+        res.status(200).send();
+    } else {
+        next();
+    }
+});
 app.use(cors({
     origin:"*",
 }));
